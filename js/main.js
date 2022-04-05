619755333ff83b1524576529d847dbfe91dcf924
ajcairel <alexanderjcairel@gmail.com>
2022-04-04 18:13:16 -0700

    Semi-working horizontal win check

--- js/main.js
+++ js/main.js
@@ -8,14 +8,17 @@ const lookup = {
     '-1': "url('https://static.wikia.nocookie.net/eroninja/images/2/23/UchihaWappen.png/revision/latest?cb=20180322234129')",
     'null': "url('https://media0.giphy.com/media/K9AnZe1fuZb68/200.gif')"
   };
-const player1Win = 4;
-const player2Win = -4;
+
+const names = {
+    '1': 'Naruto',
+    '-1': 'Sasuke'
+};
 
 /*----- app's state (variables) -----*/
 let gameBoard; // array of arrays? board that will show the connect4 game
 let turn; // Object to handle whos turn it is, alternate between player1 and player2
 let winner; // string set to null while the game is in play 
-let tieArray;
+let tieArray; // Array to cheese the way to handle a tie 
 
 /*----- cached element references -----*/
 // circles on the gameBoard
@@ -25,7 +28,10 @@ const rows = document.querySelectorAll('tr');
 const slots = document.querySelectorAll('td');
 const resetBtn = document.getElementById('reset');
 const message = document.querySelector('h3');
-// const game = [...document.querySelectorAll('td')];
+const backgroundImg = document.querySelector('div');
+const min = num => Math.max(num - 3, 0);
+const max = (num, max) => Math.min(num + 3, max);
+
 
 /*----- event listeners -----*/
 document.querySelector('table').addEventListener('click', handleMove);
@@ -46,6 +52,7 @@ function init() {
     ];
     tieArray = [];
     resetBtn.style.visibility = 'hidden';
+    winner = null;
     handleTurn();
 }
 // let turn = 1;
@@ -55,25 +62,28 @@ function handleMove(evt) {
     //console.log(cell);
     let row = evt.target.parentElement.rowIndex; // rowIndex checks tr in relation to others
     //console.log(row);
-    console.log(row * cell);
+    //console.log(row * cell);
+    if (winner !== null) return; // disallow moves if someone won
     if (cell === undefined && row === undefined) return; // so they can't click the background and change the color 
     for (let i = gameBoard.length - 1; i > -1; i--) { // start at the bottom of the rows
         if (gameBoard[i][cell] === null) {
-            console.log('Initial Array: ' + location.length);
+            //console.log('Initial Array: ' + location.length);
             location.push(rows[i].children[cell]);
-            // rows[i].children[cell].style.backgroundColor = lookup[turn]; change color
-            // rows[i].children[cell].style.backgroundImage = lookup[turn]; change image 
-            console.log('Array after push: ' + location.length);
+            // rows[i].children[cell].style.backgroundImage = lookup[turn]; //change image (moved to render)
             // location[0].style.backgroundColor = lookup[turn]; change color of cell at bottom (moved to render)
             // evt.target.style.backgroundColor = lookup[turn]; //changes where they click 
             gameBoard[i][cell]= turn; // gameBoard[i][cell] is the location of the last move played on the array of arrays
             handleTurn();
             renderMove(location); // render the move
-            //turn *= -1;
             tieArray.push('pls');
+            const minCol = min(cell);
+            const maxCol = max(i, gameBoard[0].length - 1); // 7 - 1 = 6
+            const minRow = min(cell);
+            const maxRow = max(cell, gameBoard.length - 1);
+            horizontalWinCheck(i, cell, minCol, maxCol);
             tieCheck();
             // render(i, cell);
-            console.log(`Row: ${row} Cell: ${cell}`);
+            console.log(`Row: ${row} Col: ${cell}`);
             return;
         }
     }
@@ -115,6 +125,8 @@ function tieCheck() {
     if (tieArray.length === 42) {
         resetBtn.style.visibility = 'visible';
         message.innerHTML = `There has been a stalemate!`;
+        // backgroundImg.style.backgroundImage = "url('https://i.pinimg.com/originals/88/d1/85/88d1854273d2638b7b4aeca97b27a861.jpg')";
+
     }
 }
 
@@ -122,15 +134,42 @@ function tieCheck() {
 
 // use the min to find the first spot to start checking for a win
 // relative to the last played move
-const min = num => Math.max(num - 3, 0);
-const max = (num, max) => Math.min(num + 3, max);
+
+// row, column, minimumColumn, maximumColumn
+function horizontalWinCheck(r, c, minC, maxC) { 
+    console.log('MinC: ' + minC + ' maxC: ' + maxC);
+    for (let x = r, c = minC; c <= maxC; c++) {
+        console.log('x: ' + r + ' c: ' +c  + ' minC: ' + minC + ' minR: ' + maxC);
+        if (Math.abs(gameBoard[x][c] + gameBoard[x][c+1] + 
+            gameBoard[x][c+2] + gameBoard[x][c+3] === 4)) {
+            announceWinner();
+        }
+    }
+}
+
+function announceWinner() {
+    message.innerHTML = `The winner is ${names[turn]}!`;
+    resetBtn.style.visibility = 'visible';
+
+}
+    
+
+
+   
+
+
+
 
 // const { row: focalRow, col: focalCol } = lastChecker;
 // const minCol = min(focalCol);
 // const maxCol = max(focalCol, this.colCount-1);
 // const minRow = min(focalRow);
 // const maxRow = max(focalRow, this.rowCount-1);
-
+// function checkHorizontal() {
+//     for (let r = 0; i < rows.length; r++) {
+//         for(let c = 0; )
+//     }
+// }
 
 
 
